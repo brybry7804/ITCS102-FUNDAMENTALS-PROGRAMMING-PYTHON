e0582951998014532f1b6c2d76c2ba23ce3d5ffa
Bry Echevarria <brycyne7804@gmail.com>
2025-08-22 19:47:49 +0800

    https://github.com/brybry7804/ITCS102-FUNDAMENTALS-PROGRAMMING-PYTHON.git

---
+++ activity9.py
@@ -0,0 +1,7 @@
+username = 'bry'
+password = 'pinakamaganda'
+
+print(username == 'bry')
+print((username == 'bry') and (password == 'pinakamaganda'))
+print((username == 'bry') or (password == 'pinakamaganda'))
+print (not((username == 'bry') or (password == 'pinakamaganda')))
