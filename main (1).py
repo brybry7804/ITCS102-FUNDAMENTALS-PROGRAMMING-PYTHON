7865a271f3cdb0ec0f12e8267ef31c05d2555141
brybry7804 <brycyne7804@gmail.com>
2025-09-17 15:40:55 +0800

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,7 @@
+print("COUNTDOWN TIMER SIMULATOR")
+time = int(input("Enter the starting number for coundown:"))
+
+print("Countdown started:")
+for i in range (time, 0, -1):
+    print(i)
+print("liftoff!")
