be8baa504f204b59639d590cf46a8d66c2d88f7c
Bry Echevarria <brycyne7804@gmail.com>
2025-11-16 23:24:34 +0800

    initial commit

---
+++ activity23.py
@@ -0,0 +1,25 @@
+# months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+# months.append('Jan')
+# print(months)
+# months.pop()
+# print(months)
+
+
+# name = 'Bry Echevarria'
+# for s in name:
+#     print(s)
+
+# s = ['Bry', 'Francyne', 'Echevarria']
+
+# print('Firstname is', s[0])
+
+name = 'Jungkook'
+for access in name[5 : : -1]:
+    print(access)
+
+name1 = 'Jungkook'
+e = (list(name1))
+print(e)
+e.reverse()
+print(e)
+print(e[3])
